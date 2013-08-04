ce0916f9fe5b934df5245aef8f280a05c7a0f245
Opi Danihelka <opicacek@seznam.cz>
2013-08-04 02:47:17 +0200

    Usage of node-static.

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	static = require('node-static'); // for serving files
+
+// This will make all the files in the current folder
+// accessible from the web
+var fileServer = new static.Server('./');
+
+// This is the port for our web server.
+// you will need to go to http://localhost:8080 to see it
+app.listen(8080);
+
+// If the URL of the socket server is opened in a browser
+function handler (request, response) {
+	request.addListener('end', function () {
+		fileServer.serve(request, response); // this will return the correct file
+	});
+}
+
+//TODO old version, can be deleted now
+/*
 var app = require('http').createServer(handler)
 	, io = require('socket.io').listen(app)
 	, fs = require('fs')
+	//, client = require('./js/client.js')
 
 app.listen(8080);
 
@@ -16,7 +38,7 @@ function handler (req, res) {
 		res.end(data);
 	});
 }
-
+*/
 var bro_list = {};
 
 io.sockets.on('connection', function (socket) {
