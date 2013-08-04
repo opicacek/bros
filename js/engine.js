5b0099886e561b07329f4581d6457a9baecab8b1
Opi Danihelka <opicacek@seznam.cz>
2013-08-04 14:46:04 +0200

    Smooth movement of Bro.

--- js/engine.js
+++ js/engine.js
@@ -2,5 +2,17 @@ function Engine() {
 	
 }
 Engine.prototype.moveBro = function(bro) {
-	bro.pos = bro.goal_pos;
+	//bro.pos = bro.goal_pos;
+	
+	if (bro.pos[0] < bro.goal_pos[0]) {
+		bro.pos[0] += 1;
+	} else if (bro.pos[0] > bro.goal_pos[0]) {
+		bro.pos[0] -= 1;
+	}
+	
+	if (bro.pos[1] < bro.goal_pos[1]) {
+		bro.pos[1] += 1;
+	} else if (bro.pos[1] > bro.goal_pos[1]) {
+		bro.pos[1] -= 1;
+	}
 }
